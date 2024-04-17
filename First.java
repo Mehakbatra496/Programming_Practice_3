0f487e32bc30d7d550ff03f94d7787bafe16bda7
Mehak <mehakatra496@gmail.com>
2024-04-17 09:40:05 +0530

    Two files made manually

---
+++ First.java
@@ -0,0 +1,7 @@
+class Abc
+{
+    public static void main(String args[])
+    {
+        System.out.println("This is first class made into Programmimg practice folder maually by me")
+    }
+}
---
+++ Second.java
@@ -0,0 +1,7 @@
+class Abc
+{
+    public static void main(String args[])
+    {
+        System.out.println("This is second class made into Programmimg practice folder maually by me")
+    }
+}
